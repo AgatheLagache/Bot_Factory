88e14798a14439ff899b389a2a8ec9041d1d8708
Martin Hubert <martinhub92@gmail.com>
2021-01-17 02:55:05 +0100

    world spawn with unified launch file

--- install/share/common-lisp/ros/poppy_msgs/srv/_package_ReachTarget.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package poppy_msgs-srv)
-(cl:export '(TARGET-VAL
-          TARGET
-          DURATION-VAL
-          DURATION
-))
--- install/share/common-lisp/ros/poppy_torso_control/msg/_package_Trajectory.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package poppy_torso_control-msg)
-(cl:export '(JOINT-VAL
-          JOINT
-          TRAJECTORY-VAL
-          TRAJECTORY
-))
