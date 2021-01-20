81ed21108807f9e3ec61ddd9b2f8215363a9f1b6
Martin Hubert <martinhub92@gmail.com>
2021-01-20 11:42:27 +0100

    merged production folders

--- src/production_chain/scripts/ergo_control.py
+++ src/production_chain/scripts/ergo_control.py
@@ -8,30 +8,51 @@ from geometry_msgs.msg import Pose, Quaternion
 from math import radians, cos, sin
 import tf_conversions as transform
 from std_srvs.srv import SetBool
-from production_chain.srv import ErgoControl, ErgoControlResponse
 
-def rotate_to(req):
-#	rospy.init_node('ergo_node')
-	print("\tgoing to z: %f degree: %f"%(req.z, req.degree))
-	commander = MoveGroupCommander("arm")
+commander = MoveGroupCommander("arm")
+
+def close_gripper(close):
+	rospy.wait_for_service('/ergo_jr/close_gripper')
+	try:
+		closeGripper = rospy.ServiceProxy('/ergo_jr/close_gripper', SetBool)
+		return closeGripper(close)
+	except rospy.ServiceException as e:
+		print("Service call failed: %s"%e)
+
+def rotate_to(degree, z):
+	current_pose = commander.get_current_pose().pose
+	print(current_pose)
 	cube_offset = 0.21
 	pose = geometry_msgs.msg.Pose()
-	pose.orientation = Quaternion(*transform.transformations.quaternion_from_euler(0, 0, radians(req.degree)))
-	pose.position.x = cos(radians(req.degree - 90)) * cube_offset
-	pose.position.y = sin(radians(req.degree - 90)) * cube_offset
-	pose.position.z = req.z
+	pose.orientation = Quaternion(*transform.transformations.quaternion_from_euler(0, 0, radians(degree)))
+	pose.position.x = cos(radians(degree - 90)) * cube_offset
+	pose.position.y = sin(radians(degree - 90)) * cube_offset
+	pose.position.z = z
 
-	commander.set_pose_target(pose)
-	plan = commander.go(wait=True)
-	commander.stop()
-	commander.clear_pose_targets()
-	return ErgoControlResponse(plan)
+	commander.set_pose_target(pose) #envoie la pose au commander
+	plan = commander.go(wait=True) #éxécute le mouvement avec attente
+	commander.stop() #force l'arrêt du mouvement pour plus de sécurité
+	commander.clear_pose_targets() #force le nettoyage des objectifs du commander pour plus de sécurité
 
-def rotate_to_server():
-	rospy.init_node('rotate_to_server')
-	s = rospy.Service('ergo_control', ErgoControl, rotate_to)
-	rospy.spin()
+def main():
+	rospy.init_node('ergo_node')
+
+	rotate_to(0, 0.2) #
+
+#	close_gripper(False)
+	rotate_to(25, 0.1) # TODO: obstacles
+	rotate_to(25, 0.02)
+#	close_gripper(True)
+	rotate_to(25, 0.1)#
+
+#	time.sleep(2)
+
+#	rotate_to(90, 0.02)#
+#	close_gripper(False)
+#	rotate_to(90, 0.1) # TODO: obstacles
+#	rotate_to(0, 0.20)#
+#	close_gripper(True)
 
 if __name__ == "__main__":
-	rotate_to_server()
+	main()
 
