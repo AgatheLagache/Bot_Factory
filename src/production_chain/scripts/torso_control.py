cf13f468e0b7c250fb82ac7edbc77d55aea96d00
Martin Hubert <martinhub92@gmail.com>
2021-01-22 10:23:57 +0100

    added torso pose

--- src/production_chain/scripts/torso_control.py
+++ src/production_chain/scripts/torso_control.py
@@ -10,7 +10,7 @@ import tf_conversions as transform
 rospy.init_node("test_sub")
 
 commander = MoveGroupCommander("r_arm")
-print(commander.get_current_pose().pose)
+print(commander.get_current_joint_values())
 cp = Pose()
 
 cp.position.x = 0.000
@@ -22,13 +22,20 @@ cp.orientation.y = 0.0067
 cp.orientation.z = 0.0066
 cp.orientation.w = 0.6762
 
+joints = commander.get_current_joint_values()
+
+# r_shoulder_y; r_shoulder_x; r_arm_z; r_elbow_y
+joints = [0,0,0,0]
+
+
+commander.set_joint_value_target(joints)
 #print(cp)
 
 #commander.set_orientation_target(cp.orientation)
-commander.set_joint_value_target(cp, True)
+#commander.set_joint_value_target(cp, True)
 
 #commander.set_pose_target(cp) #envoie la pose au commander
-plan = commander.go(wait=True) #éxécute le mouvement avec attente
+#plan = commander.go(wait=True) #éxécute le mouvement avec attente
 commander.stop() #force l'arrêt du mouvement pour plus de sécurité
 commander.clear_pose_targets() #force le nettoyage des objectifs du commander pour plus de sécurité
 
